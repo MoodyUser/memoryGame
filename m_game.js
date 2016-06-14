46f774abc0f15a092da533595bedd9110907af6e
D3V0P5 <s@localhost.localdomain>
2016-06-14 15:26:32 +0300

    אליך חנוך

--- m_game.js
+++ m_game.js
@@ -1,16 +1,19 @@
 /**
  * Created by yonim on 14/06/16.
  */
+ 
 class Card {
     constructor(pic_url) {
-        this.x = 0;
-        this.y = 0;
+     //   this.x = 0;
+     //   this.y = 0;
         this.pic_url = pic_url;
     }
 
     getAsHtml() {
-        return "<span class=' tile " +
-            this.pic_url + "'></span>"
+    	return `<span class="${this.pic_url}"></span>` ; 
+    	
+       /* return "<span class=' tile " +
+            this.pic_url + "'></span>" */
 
     }
 }
@@ -18,10 +21,33 @@ class Card {
 
 
 
-let content =
+let content1 = [];
 
 
-    $("content").html(
-
-
-    )
+let ims = [
+	'glyphicon glyphicon-magnet',
+	'glyphicon glyphicon-align-center',
+	'glyphicon glyphicon-home',
+	'glyphicon glyphicon-eur',
+	'glyphicon glyphicon-cog' ,	
+	'glyphicon glyphicon-picture',
+	'glyphicon glyphicon-eye-open',
+	'glyphicon glyphicon-hand-up',
+	'glyphicon glyphicon-collapse-up',
+	'glyphicon glyphicon-knight',
+	'glyphicon glyphicon-lamp' ,
+	] ;
+	
+	
+	
+	
+  for (var i = 0; i < ims.length; i++) {
+  let temp = new Card(ims[i])  ;
+  let temp1 = new Card(ims[i])  ;
+  //let ctx = document.getElementById('canvas').getContext('2d');
+	$("#can").append("<h1>lalala</h1>");
+	//alert($("#can")) ; 
+  //temp.getAsHtml()) ; 
+  //alert(temp.getAsHtml())
+  //content.push(temp1)
+  }
